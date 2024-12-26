aab3f994d8d373d6fafca8b3d68ed8f29b0049ac
Davi Emanuel Nogueira <90726774+daviEmanuelNogueira@users.noreply.github.com>
2024-12-26 01:30:06 -0300

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -8,19 +8,19 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/notifications", async (Notification notification) =>
+app.MapPost("/notification", async (Notification notification) =>
 {
     var factory = new ConnectionFactory { HostName = "rbq", Port = 5672 };
     using var connection = await factory.CreateConnectionAsync();
     using var channel = await connection.CreateChannelAsync();
 
-    await channel.QueueDeclareAsync(queue: "reap", durable: true, exclusive: false, autoDelete: false,
+    await channel.QueueDeclareAsync(queue: "fila", durable: true, exclusive: false, autoDelete: false,
         arguments: null);
 
     var message = JsonSerializer.Serialize(notification);
     var body = Encoding.UTF8.GetBytes(message);
 
-    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "reap", body: body);
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "fila", body: body);
 
     return Results.Created("/notifications", notification);
 });
